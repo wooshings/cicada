d8132b84094fde4f17c3ee46f16cfc1cb4af4840
wooshdude <parker@cline.cc>
2025-03-30 12:23:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cicada',
-    version='0.1.9',
+    version='0.1.10',
     url='https://github.com/wooshdude/cicada',
     author='wooshings',
     description=(
