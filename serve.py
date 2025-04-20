b3b800fc2063dc93e86173d0146df9e90882b492
wooshdude <parker@cline.cc>
2025-04-20 16:13:39 -0700

    update serve

--- serve.py
+++ serve.py
@@ -1,14 +1,14 @@
 import argparse
 import os
-import sys
 
 parser = argparse.ArgumentParser(description="Generate Service File.")
 parser.add_argument("name", help="Service name")
+parser.add_argument("--apply", action="store_true", help="Apply service")
 
 args = parser.parse_args()
 
 
-def main():
+def create():
     if os.path.exists(f"{args.name}.service"):
         os.remove(f"{args.name}.service")
     with open(f"{args.name}.service", "w") as f:
@@ -28,5 +28,17 @@ User=pi
 WantedBy=multi-user.target''')
 
 
+def enable():
+    if not args.apply:
+        return
+
+    os.system(
+        f"sudo mv {args.name}.service /etc/systemd/system/{args.name}.service")
+    os.system("sudo systemctl daemon-reload")
+    os.system(f"sudo systemctl enable {args.name}.service")
+    os.system(f"sudo systemctl start {args.name}.service")
+
+
 if __name__ == "__main__":
-    main()
+    create()
+    enable()
