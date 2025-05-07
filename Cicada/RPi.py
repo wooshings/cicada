751da8e199cacb4285e2edfd23db32cb63b7ee72
wooshdude <parker@cline.cc>
2025-05-07 16:06:01 -0700

    decouple button inputs from pin

--- Cicada/RPi.py
+++ Cicada/RPi.py
@@ -1,34 +1,48 @@
+import RPi.GPIO as GPIO
+import sys
+import importlib.util
 import paho.mqtt.client as mqtt
 from time import sleep
 import asyncio
-import RPi.GPIO as GPIO
+
+try:
+    importlib.util.find_spec("RPi.GPIO")
+except ImportError:
+    import fake_rpi
+    sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
+    sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO  # Fake GPIO
 
 processes = []
+
+
 def add_process():
     def wrapper(func):
         processes.append(func)
         return func
     return wrapper
 
+
 async def run_process():
     for p in processes:
         p()
 asyncio.run(run_process())
 
+
 class Nymph():
     def __init__(self, host: str, port: int, topics: list) -> None:
         self.host = host
         self.port = port
-        self.topics = topics 
+        self.topics = topics
         self.mqttc = mqtt.Client()
         self.tick_speed = 20
-        
+
         self.start()
         self._ready()
         asyncio.run(self.start_process())
 
     def start(self):
-        if self.host == "": return
+        if self.host == "":
+            return
         self.mqttc.connect(self.host, self.port, 60)
 
         self.mqttc.on_connect = self.connect_callback
@@ -58,7 +72,8 @@ class Nymph():
         pass
 
     def publish(self, topic, msg):
-        if self.host == "": print("Cannot publish using type Node. Try creating a NetworkNode instead.")
+        if self.host == "":
+            print("Cannot publish using type Node. Try creating a NetworkNode instead.")
         self.mqttc.publish(topic, msg)
 
     def connect_callback(self, client, userdata, flags, reason_code):
@@ -70,6 +85,7 @@ class Nymph():
     def message_callback(self, client, userdata, msg):
         self._on_message(msg)
 
+
 class Property():
     def __init__(self, default) -> None:
         self.default = default
@@ -77,15 +93,17 @@ class Property():
 
     def reset(self):
         self.value = self.default
-    
+
     def get_value(self):
         return str(self.value)
+
 
 def NetworkNode(host="localhost", port=1883, topics=[]):
     def wrapper(cls):
         new_node = cls(host, port, topics)
         return new_node
     return wrapper
+
 
 def Node():
     def wrapper(cls):
@@ -113,12 +131,16 @@ class Pin():
             self.on()
 
     def on(self):
-        if self.mode == self.IN: print(f"Pin {self.pin} is not an output."); return
+        if self.mode == self.IN:
+            print(f"Pin {self.pin} is not an output.")
+            return
         self.is_on = True
         GPIO.output(self.pin, 1)
 
     def off(self):
-        if self.mode == self.IN: print(f"Pin {self.pin} is not an output."); return
+        if self.mode == self.IN:
+            print(f"Pin {self.pin} is not an output.")
+            return
         self.is_on = False
         GPIO.output(self.pin, 0)
 
@@ -129,13 +151,10 @@ class Pin():
             self.on()
 
     def value(self):
-        if self.mode != GPIO.IN: print(f"Pin {self.pin} is not an input."); return
-        if GPIO.input(self.pin): return 1
+        if self.mode != GPIO.IN:
+            print(f"Pin {self.pin} is not an input.")
+            return
+        if GPIO.input(self.pin):
+            return 1
         return 0
-
-
-
-
-
-
 
